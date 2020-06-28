1d54695ecb336b0fa9c3adf499d8ea4a89705a49
Dean Bevis <65736551+DeanJB@users.noreply.github.com>
2020-06-28 16:52:01 -0700

    Added Trainer Class with methods: attack other trainer, change active, use potion (has inventory adjustment)

--- pokemon.py
+++ pokemon.py
@@ -14,7 +14,7 @@ class Pokemon:
         self.health -= damage
         if self.health <= 0:
             self.health = 0
-            self.is_knocked_out
+            self.is_knocked_out = True
             print("{pokemon} has fainted".format(pokemon = self.name))
         else:
             print("{pokemon} took {damage} points of damage. {pokemon} now has {health} health.".format(pokemon = self.name, damage = str(damage), health = self.health))
@@ -32,15 +32,10 @@ class Pokemon:
         elif gain == "revive potion":
             self.health += self.level
             print("{pokemon} is revived!".format(pokemon = self.name))
+            self.is_knocked_out = False
         else:
             print("Wrong potion, cannot revive {pokemon}.".format(pokemon = self.name))
-    def use_potion(self, potion):
-        if potion == "healing potion":
-            self.gain_health(20)
-        elif potion == "super potion":
-            self.gain_health(50)
-        elif potion == "revive potion":
-            self.revive(potion)
+    
     def attack(self, enemy):
         if self.poke_type == enemy.poke_type:
             print("{pokemon} attacks and deals {damage} to {enemy}.".format(pokemon = self.name, damage = self.level, enemy = enemy.name))
@@ -73,12 +68,77 @@ pikachu = Pokemon("Pikachu", 34, "Electric")
 charmander = Pokemon("Charmander", 25, "Fire")
 bulbasaur = Pokemon("Bulbasaur", 26, "Grass")
 squirtle = Pokemon("Squirtle", 24, "Water")
+caterpie = Pokemon("Caterpie", 18, "Grass")
+pidgey = Pokemon("Pidgey", 15, "Normal")
+ratata = Pokemon("Ratata", 17, "Normal")
+geodude = Pokemon("Geodude", 22, "Ground")
+vulpix = Pokemon("Vulpix", 29, "Fire")
+goldeen = Pokemon("Goldeen", 26, "Water")
+
+#poke_dex = {"pikachu": pikachu, "charmander": charmander, "squirtle": squirtle, "bulbasaur": bulbasaur, "caterpie": caterpie, "pidgey": pidgey, "ratata": ratata, "geodude": geodude, "vulpix": vulpix, "goldeen": goldeen}
+#Testing out above functionality of fighting mechanisms
+#pikachu.lose_health(20)
+#pikachu.lose_health(35)
+#print(pikachu)
+#pikachu.use_potion("revive potion")
+#print(pikachu)
+#pikachu.attack(charmander)
+#charmander.attack(squirtle)
+
+class Trainer:
+    def __init__(self, name, pokedex, inventory):
+        self.name = name
+        self.pokedex = pokedex
+        self.inventory = inventory
+        self.active = self.pokedex[0]
+    
+    def __repr__(self):
+        return "{trainer} wants to be the best trainer in the world.".format(trainer = self.name)
+
+    def attack_other_trainer(self, other_trainer):
+        their_active = other_trainer.active
+        if self.active.is_knocked_out == True:
+            print("{pokemon} cannot attack. {pokemon} is knocked out".format(pokemon = self.active.name))
+        else:
+            self.active.attack(their_active)
+    
+
+    def change_active(self, change_to):
+        print("That's enough {active}. {change} I choose you.".format(active = self.active.name, change = self.pokedex[change_to].name))
+        self.active = self.pokedex[change_to]
+    
+    def use_potion(self, potion):
+        print("{trainer} used {potion}.".format(trainer = self.name, potion = potion))
+        for use in self.inventory:
+            potion_index = self.inventory.index(potion)
+            if use == potion:
+                self.inventory.pop(potion_index)  
+        if potion == "healing potion":
+            self.active.gain_health(50)            
+        elif potion == "super potion":
+            self.active.gain_health(100)
+        elif potion == "revive potion":
+            self.active.revive(potion) 
 
 
-pikachu.lose_health(20)
-pikachu.lose_health(35)
-print(pikachu)
-pikachu.use_potion("revive potion")
-print(pikachu)
-pikachu.attack(charmander)
-charmander.attack(squirtle)
+ash = Trainer("Ash", [pikachu, charmander, squirtle], ["super potion", "potion", "revive potion"])
+print(ash)
+
+gary = Trainer("Gary", [bulbasaur, vulpix, geodude], ["potion", "revive potion"])
+
+ash.attack_other_trainer(gary)
+gary.change_active(1)
+gary.attack_other_trainer(ash)
+ash.attack_other_trainer(gary)
+gary.attack_other_trainer(ash)
+ash.change_active(2)
+ash.attack_other_trainer(gary)
+ash.attack_other_trainer(gary)
+ash.attack_other_trainer(gary)
+ash.attack_other_trainer(gary)
+ash.attack_other_trainer(gary)
+ash.attack_other_trainer(gary)
+gary.attack_other_trainer(ash)
+print(gary.inventory)
+gary.use_potion("revive potion")
+print(gary.inventory)
